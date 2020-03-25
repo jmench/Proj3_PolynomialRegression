ff9a77798824a38e45564092b4145b76064598a5
Jordan Menchen <jmench97@gmail.com>
2020-03-25 15:29:51 -0400

    final changes for submission

--- main.py
+++ main.py
@@ -26,71 +26,91 @@ def make_plot(data, title, coef):
     plt.title(title)
     plt.show()
 
+# get the value from the summation of predicted - actual
 def get_error(coef, row):
-    #set prediciton to intercept value to begin
     prediction = 0
     x = row[0]
     actual = row[1]
+    # loop through every coefficient to calculate prediction
     for i in range(len(coef)):
         prediction += coef[i] * (x ** i)
     error = prediction - actual
     return error
 
+# continuously updates weight values using gradient descent, returns final weights and MSE
 def update_coef(data, learn_rate, epochs, coef):
+    # create array of zeros for MSE tracking
     error_arr = np.zeros(epochs)
-    m = 1/len(data)
+    # get length of dataset
+    m = len(data)
+    # loop through all epochs
     for epoch in range(epochs):
+        # reset the error sum for the epoch
         sum_error = 0
+        # loop through each row in the dataset
         for row in data:
-            # Get predicted value here
+            # Get summation value here
             err = get_error(coef, row)
-            #update all coef
+            # loop through and update all weights using update formula
             for i in range(len(coef)):
                 coef[i] = coef[i] - learn_rate * (1/m) * err * (row[0]**i)
+            # add to the sum_error the squared error (MSE)
             sum_error += err**2
+        # add the sum_error to the MSE array
         error_arr[epoch] = sum_error
+    # calculate mean of MSE array and print value
     mse = round(np.mean(error_arr), 3)
-    print('The MSE of this model is ' + str(mse))
-    return coef
+    # return the final weight and mse values
+    return coef, mse
 
+# driver for program
 def main():
+    np.set_printoptions(precision=3)
+
+    # read in all csv files to np arrays
     syn1 = read_csv('./data/synthetic-1.csv')
     syn2 = read_csv('./data/synthetic-2.csv')
     syn3 = read_csv('./data/synthetic-3.csv')
 
+    # create all coef arrays for each order
     coef1 = [0.0, 0.0]
     coef2 = [0.0, 0.0, 0.0]
     coef4 = [0.0, 0.0, 0.0, 0.0, 0.0]
     coef7 = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
+    # create dictionary mapping coef array to string that defines it
     coef_arr = [[coef1, '1st order'], [coef2, '2nd order'], [coef4, '4th order'], [coef7, '7th order']]
 
+    # loops through every order for every dataset
     for i in range(len(coef_arr)):
-        if i < 3:
-            alpha = .00001
-        else:
-            alpha = .000001
-        coefs = update_coef(syn1, alpha, 100, coef_arr[i][0])
+        # set epochs and alpha values
+        epochs = 10000
+        alpha = .001
+
+        # update and plot for syn1
+        coefs, mse = update_coef(syn1, alpha, epochs, coef_arr[i][0])
         print('Weight results for syn1 and ' + coef_arr[i][1] + ':')
         weights = np.array(coefs)
-        np.around(weights, 3)
         print(weights)
+        print('The MSE of this model is ' + str(mse))
         print()
         make_plot(syn1, "Synthetic 1 - " + coef_arr[i][1], weights)
 
-        coefs = update_coef(syn2, alpha, 100, coef_arr[i][0])
+        # update and plot for syn 2
+        coefs, mse = update_coef(syn2, alpha, epochs, coef_arr[i][0])
         print('Weight results for syn2 and ' + coef_arr[i][1] + ':')
         weights = np.array(coefs)
-        np.around(weights, 3)
         print(weights)
+        print('The MSE of this model is ' + str(mse))
         print()
         make_plot(syn2, "Synthetic 2 - " + coef_arr[i][1], weights)
 
-        coefs = update_coef(syn3, alpha, 100, coef_arr[i][0])
+        # update and plot for syn 3
+        coefs, mse = update_coef(syn3, alpha, epochs, coef_arr[i][0])
         print('Weight results for syn3  and ' + coef_arr[i][1] + ':')
         weights = np.array(coefs)
-        np.around(weights, 3)
         print(weights)
+        print('The MSE of this model is ' + str(mse))
         print()
         make_plot(syn3, "Synthetic 3 - " + coef_arr[i][1], weights)
 
